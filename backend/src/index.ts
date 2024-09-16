6ea84265ed654aa0fb8d50a51d56759c9df1325e
Nuelo <akalo.emmanuel18@gmail.com>
2024-09-16 13:43:09 +0100

    feat: implement user redirect from auth0

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,19 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
-import "dotenv/config";
-import mongoose from "mongoose";
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import mongoose from 'mongoose';
+import myUserRoute from './routes/MyUserRoutes';
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to database!"));
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log('Connected to database!'));
 
 const app = express();
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get("/test", async (req: Request, res: Response) => {
-	res.json({ message: "Hello!" });
-})
+app.use("/api/my/user", myUserRoute)
 
-app.listen(7000, () => {
-	console.log("server start on localhost:7000");
-})
+app.listen(8000, () => {
+  console.log('server start on localhost:8000');
+});
